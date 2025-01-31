2e57da82fc347ad9e7304543981e28ffd65d6fa3
daye <moda1019@naver.com>
2025-01-31 21:20:28 +0900

    Separate roles and responsibilities with AppConfig

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,17 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // MemberServiceImpl에 MemoyMemberRepository처럼 구현체가 있는 게 X, MemeberRepository라는 인터페이스만 존재!
+    // 추상화에만 의존, DIP를 지키게 됨!
+    private final MemberRepository memberRepository;
 
+    // 외부에서 보면 의존관계를 주입해주는 것 같다고 해서
+    // DI (Dependency Injection) = 의존관계 주입이라고 함
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // "의존관계에 대한 고민은 외부에" 맡기고, "실행에만 집중"하면 됨
     @Override
     public void join(Member member) {
         memberRepository.save(member);
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,24 +3,29 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 인터페이스에만 의존하는 중! = 철저하게 DIP를 지키고 있음 good
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    // 이렇게 하면 구체화에 의존 X, 추상화에만 의존 O = DIP는 지킴!
-    private DiscountPolicy discountPolicy;
+    // final이 있으면 무조건 기본으로 생성자를 할당해야 함
+    // 생성자를 통해 어떤 구현 객체가 주입될지는 전혀 알 수 없는 상태
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+        // 생성자를 통해 어떤 구현객체가 주입될지는 오직 외부 (AppConfig)에 의해 정해짐
+    }
 
-    // 이렇게 하면 구체화에도 의존 O, 추상화에도 의존 O, final은 값이 할당되어 있을 때만 쓰는 거
+    // 이렇게 하면 구체화에도 의존 O, 추상화에도 의존 O -> DIP에 위배 됨 bad
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
+        // 실행하는 건 인터페이스만 보고 개발하면 되는 거임 (구체 클래스 볼 필요 X)
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
