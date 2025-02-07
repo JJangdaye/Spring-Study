f220272fb6ba2b4e8139305c2185b1441e989b66
daye <moda1019@naver.com>
2025-02-08 00:56:18 +0900

    normal method injection (not recommended)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService {
 
     // 인터페이스에만 의존하는 중! = 철저하게 DIP를 지키고 있음 good
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
 
     // final이 있으면 무조건 기본으로 생성자를 할당해야 함
     // 생성자를 통해 어떤 구현 객체가 주입될지는 전혀 알 수 없는 상태
@@ -21,6 +21,12 @@ public class OrderServiceImpl implements OrderService {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
         // 생성자를 통해 어떤 구현객체가 주입될지는 오직 외부 (AppConfig)에 의해 정해짐
+    }
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
     }
 
     // 이렇게 하면 구체화에도 의존 O, 추상화에도 의존 O -> DIP에 위배 됨 bad
