d993f0f52e6799cca0c3431ebe398dbc402eec44
daye <moda1019@naver.com>
2025-02-11 07:16:47 +0900

    Autowired Field name matching when multiple beans

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,13 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+
 public class OrderServiceImpl implements OrderService {
 
     // 인터페이스에만 의존하는 중! = 철저하게 DIP를 지키고 있음 good
     // @RequiredArgsConstrutor가 final 붙은 애 보고 알아서 생성자를 만들어줌
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
     // 이렇게 하면 구체화에도 의존 O, 추상화에도 의존 O -> DIP에 위배 됨 bad
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
